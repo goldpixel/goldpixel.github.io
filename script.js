50c03bf3493c2471f05bdbb240f8ab724579115a
goldpixel <y@kimyonghun.com>
2014-05-09 00:43:22 +0900

    .
    .

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-$(document).ready(function() {
-   $('div').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('div').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('div').click(function() {
-       $(this).toggle(1000);
-   }); 
-});
+var my_canvas = document.getElementById("canvas");
+var ctx = my_canvas.getContext("2d");
+
+var img = new Image();
+img.src = "jn.png";
+
+img.onload = function(){
+  ctx.drawImage(img, 0, 0);
+
+  var x = [];
+  var y = [];
+
+  for(var i = 0; i < 100; i++){
+    x[i] = Math.floor(Math.random()*1000);
+    y[i] = Math.floor(Math.random()*600);
+  }
+
+  ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
+    
+    for(var i = 0; i < 100; i++){
+    ctx.beginPath();
+    ctx.arc(x[i], y[i], 10, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
