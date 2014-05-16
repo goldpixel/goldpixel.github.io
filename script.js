689f8bc66c84b838292c7955e482ede2343279ba
goldpixel <y@kimyonghun.com>
2014-05-17 01:17:59 +0900

    .
    .

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var ctx = my_canvas.getContext("2d");
 var lat = "37.575674";
 var lng = "126.976713";
 var zm = 15;
-var mapURL;
-var mapImg;
+var mapURL = "http://maps.google.com/maps/api/staticmap?center="+lat+","+lng+"&zoom=" + zm +"&size=640x480&sensor=false&key=AIzaSyCBoXkz703lOWgwS-pfwtKoOnb6eWiFnUc";
+var mapImg.src = mapURL;
 
 
 function getMap(){
